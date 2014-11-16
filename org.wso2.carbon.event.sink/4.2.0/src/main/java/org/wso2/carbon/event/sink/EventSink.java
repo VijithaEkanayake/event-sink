99360584ab97b2a92e2a95b4074c3e6f8175ef87
Lahiru Chandima <lahiruchandima@Lahirus-MacBook-Pro.local>
2014-11-16 10:43:36 +0530

    Added LoadBalancingDataPublisher to EventSink
    Signed-off-by: Lahiru Chandima <lahiruchandima@Lahirus-MacBook-Pro.local>

--- org.wso2.carbon.event.sink/4.2.0/src/main/java/org/wso2/carbon/event/sink/EventSink.java
+++ org.wso2.carbon.event.sink/4.2.0/src/main/java/org/wso2/carbon/event/sink/EventSink.java
@@ -1,9 +1,12 @@
 package org.wso2.carbon.event.sink;
 
+import org.wso2.carbon.databridge.agent.thrift.lb.LoadBalancingDataPublisher;
+
 public interface EventSink {
     public String getName();
     public String getUsername();
     public String getPassword();
     public String getReceiverUrlSet();
     public String getAuthenticationUrlSet();
+    public LoadBalancingDataPublisher getLoadBalancingDataPublisher();
 }
