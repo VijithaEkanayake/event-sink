376f33d267ff05aaa637d69c8f40f9f9e6bd2714
Lahiru Chandima <lahiruchandima@Lahirus-MacBook-Pro.local>
2014-11-18 13:18:12 +0530

    Formatting xml of event sink
    Signed-off-by: Lahiru Chandima <lahiruchandima@Lahirus-MacBook-Pro.local>

--- org.wso2.carbon.event.sink.config/4.2.0/src/main/java/org/wso2/carbon/event/sink/config/EventSinkXmlWriter.java
+++ org.wso2.carbon.event.sink.config/4.2.0/src/main/java/org/wso2/carbon/event/sink/config/EventSinkXmlWriter.java
@@ -2,12 +2,18 @@ package org.wso2.carbon.event.sink.config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.event.formatter.core.exception.EventFormatterConfigurationException;
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.w3c.dom.Document;
 import org.wso2.carbon.event.sink.config.services.utils.CryptographyManager;
 import org.wso2.carbon.utils.CarbonUtils;
 import org.wso2.carbon.utils.ServerConstants;
-import org.wso2.carbon.event.formatter.core.internal.util.helper.*;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.util.ArrayList;
 
@@ -16,45 +22,55 @@ import java.util.ArrayList;
  */
 public class EventSinkXmlWriter {
     private static final Log log = LogFactory.getLog(EventSinkXmlWriter.class);
-
     String carbonHome = System.getProperty(ServerConstants.CARBON_HOME);
     String filePath = carbonHome + File.separator + "repository" + File.separator + "conf" + File.separator;
     String tenantFilePath = CarbonUtils.getCarbonTenantsDirPath();
     EventSinkConfigXml eventSinkConfigXml = new EventSinkConfigXml();
     CryptographyManager cryptographyManager = new CryptographyManager();
 
-    public void writeEventSink(EventSink eventSink ){
-
+    public void writeEventSink(EventSink eventSink) {
         try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(filePath,eventSink.getName()+".xml")));
-            String unformatted = new XmlFormatter().format(eventSinkConfigXml.buildEventSink(eventSink.getUsername(),encryptAndBase64Encode(eventSink.getPassword()),eventSink.getReceiverUrl(),eventSink.getAuthenticatorUrl()).toString());
-            bufferedWriter.write(unformatted);
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(filePath, eventSink.getName() + ".xml")));
+            String unFormattedXml = eventSinkConfigXml.buildEventSink(eventSink.getUsername(), encryptAndBase64Encode(eventSink.getPassword()), eventSink.getReceiverUrl(), eventSink.getAuthenticatorUrl()).toString();
+
+            ///formatting xml
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            InputSource is = new InputSource(new StringReader(unFormattedXml));
+            final Document document = db.parse(is);
+            OutputFormat format = new OutputFormat(document);
+            format.setLineWidth(100);
+            format.setIndenting(true);
+            format.setIndent(4);
+            Writer out = new StringWriter();
+            XMLSerializer serializer = new XMLSerializer(out, format);
+            serializer.serialize(document);
+            ///
+
+            bufferedWriter.write(out.toString());
 
             bufferedWriter.flush();
             bufferedWriter.close();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            log.error("Failed to open file to write event sink. File: " + filePath + ", error: " + e.getLocalizedMessage());
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (EventFormatterConfigurationException e) {
-            e.printStackTrace();
+            log.error("Failed to write event sink to file. File: " + filePath + ", error: " + e.getLocalizedMessage());
+        } catch (ParserConfigurationException e) {
+            log.error("Internal error occurred while writing event sink. Failed to format XML. error: " + e.getLocalizedMessage());
+        } catch (SAXException e) {
+            log.error("Internal error occurred while writing event sink. Invalid XML. error: " + e.getLocalizedMessage());
         }
-
     }
 
 
+    public ArrayList<String> getEventSinkNames() {
 
 
-
-
-    public ArrayList<String> getEventSinkNames(){
-
-
-        ArrayList<String> eventSinkList=null;
+        ArrayList<String> eventSinkList = null;
         File dir = new File(filePath);
         File[] directoryListing = dir.listFiles();
-        if (directoryListing != null){
-            for(File eventSink : directoryListing){
+        if (directoryListing != null) {
+            for (File eventSink : directoryListing) {
                 eventSinkList.add(eventSink.getName());
             }
         }
